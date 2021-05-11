3ef4a388a60850a37352e79329fdadc63543d787
KimUJin3359 <wj3359@naver.com>
2021-05-11 22:51:33 +0900

    fit to each monitor size

--- game.c
+++ game.c
@@ -7,15 +7,18 @@ void make_map(MAP *game)
 
 	game->ny = 1;
 	game->nx = 1;
+	game->map = (char **)malloc(sizeof(char *) * game->map_y);
+	for (int i = 0; i < game->map_y; i++)
+		game->map[i] = (char *)malloc(sizeof(char) * game->map_x);
 	game->hp = 100;
 	// make a basic setting
-	for (int i = 0; i < Y; i++)
+	for (int i = 0; i < game->map_y; i++)
 	{
 		int j;
 
-		for (j = 0; j < X; j++)
+		for (j = 0; j < game->map_x; j++)
 		{
-			if (i == 0 || i == Y - 1 || j == 0 || j == X - 1)
+			if (i == 0 || i == game->map_y - 1 || j == 0 || j == game->map_x - 1)
 				game->map[i][j] = '#';
 			else
 				game->map[i][j] = ' ';
@@ -25,37 +28,37 @@ void make_map(MAP *game)
 	srand((unsigned int)time(NULL));	
 
 	// make a random wall
-	for (int i = 0; i < Y; i++)
+	for (int i = 0; i < game->map_x; i++)
 	{
-		t_y = rand() % (Y - 2) + 1;
-		t_x = rand() % (X - 2) + 1;
+		t_y = rand() % (game->map_y - 2) + 1;
+		t_x = rand() % (game->map_x - 2) + 1;
 		if (t_y != 1 && t_x != 1)
 			game->map[t_y][t_x] = '#';
 	}
 
 	// make a random apple
-	for (int i = 0; i < Y / 5; i++)
+	for (int i = 0; i < game->map_x / 4; i++)
 	{
-		t_y = rand() % (Y - 2) + 1;
-		t_x = rand() % (X - 2) + 1;
+		t_y = rand() % (game->map_y - 2) + 1;
+		t_x = rand() % (game->map_x - 2) + 1;
 		if (t_y != 1 && t_x != 1)
 			game->map[t_y][t_x] = 'a';
 	}
 
 	// make a random thorn
-	for (int i = 0; i < Y; i++)
+	for (int i = 0; i < (game->map_x * 2); i++)
 	{
- 		t_y = rand() % (Y - 2) + 1;
-		t_x = rand() % (X - 2) + 1;
+ 		t_y = rand() % (game->map_y - 2) + 1;
+		t_x = rand() % (game->map_x - 2) + 1;
 		if (t_y != 1 && t_x != 1)
 			game->map[t_y][t_x] = '^';
 	}
 
 	// make a random monster
-	for (int i = 0; i < Y / 5; i++)
+	for (int i = 0; i < game->map_x / 4; i++)
 	{
-		t_y = rand() % (Y - 2) + 1;
-		t_x = rand() % (X - 2) + 1;
+		t_y = rand() % (game->map_y - 2) + 1;
+		t_x = rand() % (game->map_x - 2) + 1;
 		if (t_y != 1 && t_x != 1)
 			game->map[t_y][t_x] = 'M';
 	}
@@ -63,8 +66,8 @@ void make_map(MAP *game)
 	// make a random holy grail
 	while (true)
 	{
-		t_y = rand() % (Y - 2) + 1;
-		t_x = rand() % (X - 2) + 1;
+		t_y = rand() % (game->map_y - 2) + 1;
+		t_x = rand() % (game->map_x - 2) + 1;
 		if (t_y != 1 && t_x != 1)
 		{
 			game->map[t_y][t_x] = 'Y';
@@ -77,9 +80,9 @@ void make_map(MAP *game)
 void print_map(MAP game)
 {
 	clear();
-	for (int y = 0; y < Y; y++)
+	for (int y = 0; y < game.map_y; y++)
 	{
-		for (int x = 0; x < X; x++)
+		for (int x = 0; x < game.map_x; x++)
 		{
 			if (y==game.ny && x==game.nx)
 				printw("%s", "🤺");
@@ -98,17 +101,35 @@ void print_map(MAP game)
 		}
 		printw("\n");
 	}
-	printw("\n\n\n HP : %d", game.hp);
+	printw("\n HP : %d", game.hp);
 	refresh();
 }
 
 // judge if movable
 bool is_movable(MAP game, int n_y, int n_x)
 {
-	if (n_y < 0 || n_y >= Y || n_x < 0 || n_x >= X)
+	if (n_y < 0 || n_y >= game.map_y || n_x < 0 || n_x >= game.map_x)
 		return (false);
 	else if (game.map[n_y][n_x] == '#')
 		return (false);
+	return (true);
+}
+
+bool game_end(MAP game, int n)
+{
+	clear();
+	refresh();
+	usleep(1000 * 500);
+	move(game.map_y / 2, game.map_x / 2);
+	if (n == 1)
+		printw("GAME OVER\n");
+	else
+	{
+		printw("HP : %d\n", game.hp);
+		printw("WIN\n");
+	}
+	refresh();
+	sleep(1);
 	return (true);
 }
 
@@ -116,50 +137,19 @@ bool is_movable(MAP game, int n_y, int n_x)
 bool touch_MorY(MAP game)
 {
 	if (game.map[game.ny][game.nx] == 'M')
-	{	
-		usleep(1000 * 500);
-		clear();
-		refresh();
-		move(10, 20);
-		printw("Game Over\n");
-		refresh();
-		sleep(1);
-		return (true);
-	}
+		return (game_end(game, 1));
 	else if (game.map[game.ny][game.nx] == 'Y')
-	{
-		usleep(1000 * 500);
-		clear();
-		refresh();
-		move(10, 20);
-		printw("WIN\n");
-		move(11, 20);
-		printw("HP : %d\n", game.hp);
-		refresh();
-		sleep(1);
-		return (true);
-	}
+		return (game_end(game, 0));
 	else if (game.map[game.ny][game.nx] == '^')
-	{
-		if (game.hp == 0)
-		{
-			usleep(1000 * 500);
-			clear();
-			refresh();
-			move(10, 20);
-			printw("Game Over\n");
-			refresh();
-			sleep(1);
-			return (true);
-		}
-	}
+		return (game_end(game, 1));
 	return (false);
 }
 
 // meet apple, or thorn
 void touch_AorT(MAP *game)
 {
-	if (game->map[game->ny][game->nx] == '^') game->hp -= 10;
+	if (game->map[game->ny][game->nx] == '^') 
+		game->hp -= 10;
 	else if (game->map[game->ny][game->nx] == 'a')
 	{
 		game->hp += 10;
--- monster.c
+++ monster.c
@@ -3,14 +3,14 @@
 extern pthread_mutex_t mlock;
 int dy[4] = { -1, 1, 0, 0 };
 int dx[4] = { 0, 0, -1, 1 };
-char copy[Y][X + 1];
+char copy[200][100];
 
-void copy_map(char map[Y][X + 1])
+void copy_map(MAP* game)
 {
-	for (int i = 0; i < Y; i++)
+	for (int i = 0; i < game->map_y; i++)
 	{
-		for (int j = 0; j <= X; j++)
-			copy[i][j] = map[i][j];
+		for (int j = 0; j <= game->map_x; j++)
+			copy[i][j] = game->map[i][j];
 	}
 }
 
@@ -19,25 +19,27 @@ void *monster_move(void *ptr)
 	int temp;
 
 	MAP *game = (MAP *)ptr;
-	for (int i = 0; i < Y; i++)
+	/*
+	for (int i = 0; i < game->map_y; i++)
 	{
-		for (int j = 0; j <= X; j++)
+		for (int j = 0; j <= game->map_x; j++)
 			printf("%c ", game->map[i][j]);
 		printf("\n");
 	}
-	copy_map(game->map);
+	*/
+	copy_map(game);
 	srand((unsigned int)time(NULL));
 	while (1)
 	{
-		for (int i = 0; i < Y; i++)
+		for (int i = 0; i < game->map_y; i++)
 		{
-			for (int j = 0; j < X; j++)
+			for (int j = 0; j < game->map_x; j++)
 			{
 				if (game->map[i][j] == 'M')
 				{
 					temp = rand() % 4;
 					// map size check && wall check
-					if (i + dy[temp] >= 0 && i + dy[temp] < Y && j + dx[temp] >=0 && j + dx[temp] < X && game->map[i + dy[temp]][j + dx[temp]] != '#')
+					if (i + dy[temp] >= 0 && i + dy[temp] < game->map_y && j + dx[temp] >=0 && j + dx[temp] < game->map_x && game->map[i + dy[temp]][j + dx[temp]] != '#')
 					{
 						game->map[i + dy[temp]][j + dx[temp]] = 'M';
 						pthread_mutex_lock(&mlock);
